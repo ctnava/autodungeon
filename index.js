b0cae047f1587b0adb4899709098b11e52fad984
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-10-19 15:37:10 -0500

    arg details for the layman

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const dungeon = require('./utils/dungeon.js');
 const dice = require('./utils/dice.js');
 
-dungeon.generate();
+dungeon.generate(
+  (mobLimit = 1), // perRoom
+  (trapLimit = 1), // perRoom
+  (deployAllMobs = true),
+  (deployAllTraps = true),
+  (mobs = ['zombie', 'goblin', 'golem']), // min 3
+  (traps = ['boulder', 'acidPit', 'lTurret']), // min 3
+  (bosses = ['ghostSharks', 'mummyWizard', 'tRex']),
+  (roomCount = 6) // min 3
+);
